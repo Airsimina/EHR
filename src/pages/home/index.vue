4a1ab3fbc73509c6710ab7e0c9e1c8f5826e0a4e
lijiao1185 <546231429@qq.com>
2020-02-27 11:41:46 +0800

    优化跳转

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -170,16 +170,18 @@ export default {
           this.OAurl = ''
           break
         case 'Q3':
-          this.OAurl = `http://mob.huaxincem.com/appPI/weixinQY/oauth2/home.do?WXQY_REQUEST=1&isShowBack=http://mob.huaxincem.com/ehr/mobile/?userId=${this.userId}#/home`
+          this.OAurl = encodeURI(`http://mob.huaxincem.com/appPI/weixinQY/oauth2/home.do?WXQY_REQUEST=1&isShowBack=http://mob.huaxincem.com/ehr/mobile/?userId=${this.userId}#/home`)
           // this.OAurl = 'https://mob.huaxincem.com/appPI/weixinQY/ui/index.html?userId=' + this.userId
           break
         case 'PRO_DEV':
-          this.OAurl = `http://mob.huaxincem.com/appPI/weixinQY/oauth2/home.do?WXQY_REQUEST=1&isShowBack=http://mob.huaxincem.com/ehr/mobile/?userId=${this.userId}#/home`
+          this.OAurl = encodeURI(`http://mob.huaxincem.com/appPI/weixinQY/oauth2/home.do?WXQY_REQUEST=1&isShowBack=http://mob.huaxincem.com/ehr/mobile/?userId=${this.userId}#/home`)
+          // this.OAurl = `http://mob.huaxincem.com/appPI/weixinQY/oauth2/home.do?WXQY_REQUEST=1&isShowBack=http://mob.huaxincem.com/ehr/mobile/?userId=${this.userId}#/home`
           // this.OAurl = 'https://mobq.huaxincem.com/appPI/weixinQY/ui/index.html?userId=' + this.userId
           break
         default:
           // dev
-          this.OAurl = `http://mobq.huaxincem.com/appPI/weixinQY/oauth2/home.do?WXQY_REQUEST=1&isShowBack=http://mob.huaxincem.com/ehr/mobile/?userId=${this.userId}#/home`
+          this.OAurl = encodeURI(`http://mobq.huaxincem.com/appPI/weixinQY/oauth2/home.do?WXQY_REQUEST=1&isShowBack=http://mob.huaxincem.com/ehr/mobile/?userId=${this.userId}#/home`)
+          // this.OAurl = `http://mobq.huaxincem.com/appPI/weixinQY/oauth2/home.do?WXQY_REQUEST=1&isShowBack=http://mob.huaxincem.com/ehr/mobile/?userId=${this.userId}#/home`
           // this.OAurl = 'https://mobq.huaxincem.com/appPI/weixinQY/ui/index.html?userId=' + this.userId
           break
       }
