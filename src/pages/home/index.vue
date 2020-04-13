82d1ff4fca359fa504dad6ac04e83b6f0b6875a1
hbtcloud/lichenxi <465832390@qq.com>
2020-04-13 15:04:21 +0800

    删除跳转

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -185,7 +185,6 @@ export default {
         `http://mob.huaxincem.com/ehr/mobile/?userId=${this.userId}#/home`
       )
       console.log(BUILD_TYPE + '环境')
-      debugger;
       switch (BUILD_TYPE) {
         case 'PRO':
           this.OAurl = 'http://pesm.huaxincem.com/appPI/weixinQY/oauth2/home.do?WXQY_REQUEST=1&isShowBack=' +
