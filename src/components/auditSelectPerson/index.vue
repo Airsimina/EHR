68e81a99f10087a6b06429d33d359390d131882f
hbtcloud/lichenxi <465832390@qq.com>
2020-04-13 10:37:59 +0800

    只让hr有会签功能

--- src/components/auditSelectPerson/index.vue
+++ src/components/auditSelectPerson/index.vue
@@ -10,61 +10,53 @@
         </template>
         <van-cell-group>
           <template v-for="(item,index) in newAssingersSelectList">
-            <van-field
-              v-model="item.showModifyAssignerName"
-              :label="item.nodeName"
-              use-button-slot
-              readonly
-            >
-              <van-button
-                slot="button"
-                size="mini"
-                type="primary"
-                v-if="!item.chooseNameModifyAssignerPerson[0].sysUsername"
-                hairline
-                plain
-                round
-                color="#518cf1"
-                @click="handleChoosePerson(index)"
-              >选择</van-button>
-              <van-button
-                slot="button"
-                size="mini"
-                type="primary"
-                round
-                plain
-                icon="plus"
-                hairline
-                color="#518cf1"
-                v-if="item.chooseNameModifyAssignerPerson[0].sysUsername"
-                @click="handleAddPerson(index)"
-              ></van-button>
-              <van-button
-                slot="button"
-                size="mini"
-                type="primary"
-                v-if="item.nodeAssignerPersons.length>1"
-                hairline
-                plain
-                round
-                color="#518cf1"
-                @click="handleChooselocalPerson(item.nodeAssignerPersons,index)"
-              >选择</van-button>
+            <van-field v-model="item.showModifyAssignerName"
+                       :label="item.nodeName"
+                       use-button-slot
+                       readonly>
+              <van-button slot="button"
+                          size="mini"
+                          type="primary"
+                          v-if="!item.chooseNameModifyAssignerPerson[0].sysUsername"
+                          hairline
+                          plain
+                          round
+                          color="#518cf1"
+                          @click="handleChoosePerson(index)">选择</van-button>
+              <van-button slot="button"
+                          size="mini"
+                          type="primary"
+                          round
+                          plain
+                          icon="plus"
+                          hairline
+                          color="#518cf1"
+                          v-if="item.chooseNameModifyAssignerPerson[0].sysUsername&&item.nodeName =='N+1'"
+                          @click="handleAddPerson(index)"></van-button>
+              <van-button slot="button"
+                          size="mini"
+                          type="primary"
+                          v-if="item.nodeAssignerPersons.length>1"
+                          hairline
+                          plain
+                          round
+                          color="#518cf1"
+                          @click="handleChooselocalPerson(item.nodeAssignerPersons,index)">选择</van-button>
             </van-field>
           </template>
         </van-cell-group>
       </van-collapse-item>
     </van-collapse>
-    <selectPersonCard v-model="showAuditPerson" @config="handleSelectConfig"></selectPersonCard>
-    <van-popup v-model="isSelect" position="bottom">
-      <van-picker
-        :columns="selectPersonList"
-        value-key="name"
-        title="审批人选择"
-        show-toolbar
-        @cancle="handleSelectCancle"
-        @confirm="handlePersonConfirm"
-      />
+    <selectPersonCard v-model="showAuditPerson"
+                      @config="handleSelectConfig"></selectPersonCard>
+    <van-popup v-model="isSelect"
+               position="bottom">
+      <van-picker :columns="selectPersonList"
+                  value-key="name"
+                  title="审批人选择"
+                  show-toolbar
+                  @cancle="handleSelectCancle"
+                  @confirm="handlePersonConfirm" />
     </van-popup>
   </div>
 </template>
@@ -75,7 +67,7 @@ export default {
   components: {
     selectPersonCard
   },
-  data() {
+  data () {
     return {
       activeNames: ['0'],
       showAuditPerson: false,
@@ -89,110 +81,110 @@ export default {
   props: {
     assingersSelectList: {
       type: Array,
-      default: function() {
+      default: function () {
         return []
       }
     },
     newAssingersSelectList: {
       type: Array,
-      default: function() {
+      default: function () {
         return []
       }
     },
     jsonData: {
       type: Object,
-      default: function() {
+      default: function () {
         return {}
       }
     }
   },
-  mounted() {
+  mounted () {
     this.init()
   },
   methods: {
-    handleSelectCancle() {
-      this.isSelect=false
+    handleSelectCancle () {
+      this.isSelect = false
     },
-    handlePersonConfirm(selectPerson) {
-      this.newAssingersSelectList[this.selectIndex].chooseNameModifyAssignerPerson.splice(0,1,selectPerson)
-      const ModifyAssignerName=this.getShowName(this.newAssingersSelectList[this.selectIndex].chooseNameModifyAssignerPerson)
-      this.newAssingersSelectList[this.selectIndex].showModifyAssignerName=ModifyAssignerName
-      this.newAssingersSelectList[this.selectIndex].assignerName=this.newAssingersSelectList[this.selectIndex].chooseNameModifyAssignerPerson[0].name
-      this.newAssingersSelectList[this.selectIndex].assignerId=this.newAssingersSelectList[this.selectIndex].chooseNameModifyAssignerPerson[0].sysUsername
+    handlePersonConfirm (selectPerson) {
+      this.newAssingersSelectList[this.selectIndex].chooseNameModifyAssignerPerson.splice(0, 1, selectPerson)
+      const ModifyAssignerName = this.getShowName(this.newAssingersSelectList[this.selectIndex].chooseNameModifyAssignerPerson)
+      this.newAssingersSelectList[this.selectIndex].showModifyAssignerName = ModifyAssignerName
+      this.newAssingersSelectList[this.selectIndex].assignerName = this.newAssingersSelectList[this.selectIndex].chooseNameModifyAssignerPerson[0].name
+      this.newAssingersSelectList[this.selectIndex].assignerId = this.newAssingersSelectList[this.selectIndex].chooseNameModifyAssignerPerson[0].sysUsername
 
-      this.isSelect=false
+      this.isSelect = false
     },
     // 对审批人大于两个人的人员进行选择
-    handleChooselocalPerson(persons,index) {
-      this.selectIndex=index
-      this.selectPersonList=persons
-      this.isSelect=true
+    handleChooselocalPerson (persons, index) {
+      this.selectIndex = index
+      this.selectPersonList = persons
+      this.isSelect = true
     },
     // 更换申请人时触发的方法
-    handleSelectConfig(value) {
-      console.log('value',value)
-      if(!this.isAdd) {
-        this.newAssingersSelectList[this.addpersonIndex].chooseNameModifyAssignerPerson.splice(0,1,value)
+    handleSelectConfig (value) {
+      console.log('value', value)
+      if (!this.isAdd) {
+        this.newAssingersSelectList[this.addpersonIndex].chooseNameModifyAssignerPerson.splice(0, 1, value)
       } else {
         this.newAssingersSelectList[this.addpersonIndex].chooseNameModifyAssignerPerson.push(value)
       }
       // 设置传入的assignerId
-      let assignerId=''
-      this.newAssingersSelectList[this.addpersonIndex].chooseNameModifyAssignerPerson.forEach((item,index) => {
-        assignerId+=`${item.sysUsername},`
+      let assignerId = ''
+      this.newAssingersSelectList[this.addpersonIndex].chooseNameModifyAssignerPerson.forEach((item, index) => {
+        assignerId += `${item.sysUsername},`
       })
-      this.newAssingersSelectList[this.addpersonIndex].assignerId=assignerId.substring(0,assignerId.length-1)
+      this.newAssingersSelectList[this.addpersonIndex].assignerId = assignerId.substring(0, assignerId.length - 1)
       // 设置传入进来的assignerName
-      let assignerName=''
-      this.newAssingersSelectList[this.addpersonIndex].chooseNameModifyAssignerPerson.forEach((item,index) => {
-        assignerName+=`${item.name},`
+      let assignerName = ''
+      this.newAssingersSelectList[this.addpersonIndex].chooseNameModifyAssignerPerson.forEach((item, index) => {
+        assignerName += `${item.name},`
       })
-      this.newAssingersSelectList[this.addpersonIndex].assignerName=assignerName.substring(0,assignerName.length-1)
-      this.newAssingersSelectList[this.addpersonIndex].showModifyAssignerName=this.getShowName(this.newAssingersSelectList[this.addpersonIndex].chooseNameModifyAssignerPerson)
+      this.newAssingersSelectList[this.addpersonIndex].assignerName = assignerName.substring(0, assignerName.length - 1)
+      this.newAssingersSelectList[this.addpersonIndex].showModifyAssignerName = this.getShowName(this.newAssingersSelectList[this.addpersonIndex].chooseNameModifyAssignerPerson)
     },
 
-    init() {
+    init () {
       // 深拷贝传入进来的数组
-      if(this.newAssingersSelectList.length>1) {
+      if (this.newAssingersSelectList.length > 1) {
         return
       }
-      this.assingersSelectList.forEach((item,index) => {
+      this.assingersSelectList.forEach((item, index) => {
         this.newAssingersSelectList.push(item)
       })
-      this.newAssingersSelectList.forEach((item,index) => {
-        if(item.nodeAssignerPersons&&item.nodeAssignerPersons.length===0) {
-          item.chooseNameModifyAssignerPerson=[{
+      this.newAssingersSelectList.forEach((item, index) => {
+        if (item.nodeAssignerPersons && item.nodeAssignerPersons.length === 0) {
+          item.chooseNameModifyAssignerPerson = [{
             sysUsername: '',
             name: '',
             id: ''
           }]
         } else {
-          item.chooseNameModifyAssignerPerson=[item.nodeAssignerPersons[0]]
-          const ModifyAssignerName=this.getShowName(item.chooseNameModifyAssignerPerson)
-          item.showModifyAssignerName=ModifyAssignerName
-          item.assignerName=item.chooseNameModifyAssignerPerson[0].name
-          item.assignerId=item.chooseNameModifyAssignerPerson[0].sysUsername
+          item.chooseNameModifyAssignerPerson = [item.nodeAssignerPersons[0]]
+          const ModifyAssignerName = this.getShowName(item.chooseNameModifyAssignerPerson)
+          item.showModifyAssignerName = ModifyAssignerName
+          item.assignerName = item.chooseNameModifyAssignerPerson[0].name
+          item.assignerId = item.chooseNameModifyAssignerPerson[0].sysUsername
         }
       })
-      console.log('newAssingersSelectList',this.newAssingersSelectList)
+      console.log('newAssingersSelectList', this.newAssingersSelectList)
     },
-    getShowName(modifyAssignerPersons) {
-      let showName=''
-      modifyAssignerPersons.forEach((item,index) => {
-        showName+=`${item.name},`
+    getShowName (modifyAssignerPersons) {
+      let showName = ''
+      modifyAssignerPersons.forEach((item, index) => {
+        showName += `${item.name},`
       })
-      showName=showName.substring(0,showName.length-1)
+      showName = showName.substring(0, showName.length - 1)
       return showName
     },
-    handleChoosePerson(index) {
-      this.showAuditPerson=true
-      this.addpersonIndex=index // 向数组里面添加选择审批的人
-      this.isAdd=false
+    handleChoosePerson (index) {
+      this.showAuditPerson = true
+      this.addpersonIndex = index // 向数组里面添加选择审批的人
+      this.isAdd = false
     },
-    handleAddPerson(index) {
-      this.isAdd=true
-      this.showAuditPerson=true
-      this.addpersonIndex=index // 向数组里面添加选择审批的人
+    handleAddPerson (index) {
+      this.isAdd = true
+      this.showAuditPerson = true
+      this.addpersonIndex = index // 向数组里面添加选择审批的人
     }
   }
 
