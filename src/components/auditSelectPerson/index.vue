289558f3434a1d01d4ad9c9652519a63c1204a33
lichenxi <xxinso@lichenxideMacBook-Pro.local>
2020-04-17 03:15:59 +0800

    增加审批流删除功能

--- src/components/auditSelectPerson/index.vue
+++ src/components/auditSelectPerson/index.vue
@@ -8,49 +8,65 @@
             <van-icon name="question-o" />
           </div>
         </template>
+
         <van-cell-group>
           <template v-for="(item,index) in newAssingersSelectList">
-            <van-field
-              v-model="item.showModifyAssignerName"
-              :label="item.nodeName"
-              use-button-slot
-              readonly
-            >
-              <van-button
-                slot="button"
-                size="mini"
-                type="primary"
-                v-if="!item.chooseNameModifyAssignerPerson[0].sysUsername"
-                hairline
-                plain
-                round
-                color="#518cf1"
-                @click="handleChoosePerson(index)"
-              >选择</van-button>
-              <van-button
-                slot="button"
-                size="mini"
-                type="primary"
-                round
-                plain
-                icon="plus"
-                hairline
-                color="#518cf1"
-                v-if="item.chooseNameModifyAssignerPerson[0].sysUsername&&item.nodeName =='N+1'"
-                @click="handleAddPerson(index)"
-              ></van-button>
-              <van-button
-                slot="button"
-                size="mini"
-                type="primary"
-                v-if="item.nodeAssignerPersons.length>1"
-                hairline
-                plain
-                round
-                color="#518cf1"
-                @click="handleChooselocalPerson(item.nodeAssignerPersons,index)"
-              >选择</van-button>
-            </van-field>
+            <div class="my-single-cell">
+              <div class="cell-name">{{item.nodeName}}:</div>
+              <div class="cell-content">
+                <div
+                  class="single-cell-content"
+                  v-for="(tagItem,tagIndex) in item.chooseNameModifyAssignerPerson"
+                  :key="tagIndex"
+                >
+                  <van-tag
+                    type="primary"
+                    :key="tagIndex"
+                    :closeable="tagIndex > 0"
+                    color="#ffffff"
+                    text-color="rgb(81, 140, 241)"
+                    plain
+                    @close="handleTagClose(index,tagIndex)"
+                  >{{tagItem.name}}</van-tag>
+                </div>
+              </div>
+              <div class="button-container">
+                <van-button
+                  slot="button"
+                  size="mini"
+                  type="primary"
+                  v-if="!item.chooseNameModifyAssignerPerson[0].sysUsername"
+                  hairline
+                  plain
+                  round
+                  color="#518cf1"
+                  @click="handleChoosePerson(index)"
+                >选择</van-button>
+                <van-button
+                  slot="button"
+                  size="mini"
+                  type="primary"
+                  round
+                  plain
+                  icon="plus"
+                  hairline
+                  color="#518cf1"
+                  v-if="item.chooseNameModifyAssignerPerson[0].sysUsername&&item.nodeName =='N+1'"
+                  @click="handleAddPerson(index)"
+                ></van-button>
+                <van-button
+                  slot="button"
+                  size="mini"
+                  type="primary"
+                  v-if="item.nodeAssignerPersons.length>1"
+                  hairline
+                  plain
+                  round
+                  color="#518cf1"
+                  @click="handleChooselocalPerson(item.nodeAssignerPersons,index)"
+                >选择</van-button>
+              </div>
+            </div>
           </template>
         </van-cell-group>
       </van-collapse-item>
@@ -110,6 +126,14 @@ export default {
     this.init()
   },
   methods: {
+    closeTagClass(index) {
+      if(index==0) return
+      else {
+        return {
+          background: 'red'
+        }
+      }
+    },
     handleSelectCancle() {
       this.isSelect=false
     },
@@ -193,6 +217,25 @@ export default {
       this.isAdd=true
       this.showAuditPerson=true
       this.addpersonIndex=index // 向数组里面添加选择审批的人
+    },
+    handleTagClose(index,singleIndex) {
+      const chooseNameModifyAssignerPerson=this.newAssingersSelectList[index].chooseNameModifyAssignerPerson
+      chooseNameModifyAssignerPerson.splice(singleIndex,1)
+      this.$set(this.newAssingersSelectList[index],'chooseNameModifyAssignerPerson',chooseNameModifyAssignerPerson)
+      // 设置传入的assignerId
+      let assignerId=''
+      this.newAssingersSelectList[this.addpersonIndex].chooseNameModifyAssignerPerson.forEach((item,index) => {
+        assignerId+=`${item.sysUsername},`
+      })
+      this.newAssingersSelectList[this.addpersonIndex].assignerId=assignerId.substring(0,assignerId.length-1)
+      // 设置传入进来的assignerName
+      let assignerName=''
+      this.newAssingersSelectList[this.addpersonIndex].chooseNameModifyAssignerPerson.forEach((item,index) => {
+        assignerName+=`${item.name},`
+      })
+      this.newAssingersSelectList[this.addpersonIndex].assignerName=assignerName.substring(0,assignerName.length-1)
+      console.log('newAssingersSelectList',this.newAssingersSelectList)
+      this.$forceUpdate()
     }
   }
 
@@ -203,5 +246,35 @@ export default {
 ::v-deep .van-field__label {
   margin-right: 10px;
   width: auto;
+}
+::v-deep .van-collapse-item__content {
+  padding: 0;
+}
+.my-single-cell {
+  width: 100%;
+  height: 0.8rem;
+  border: 1px solid #f7f7f7;
+  line-height: 0.6rem;
+  box-sizing: border-box;
+  padding: 0.1rem 0 0.1rem 0.3rem;
+  color: #323233;
+  font-size: 0.28rem;
+  display: flex;
+  .cell-name {
+    font-size: 0.28rem;
+    color: #323233;
+    width: 1.2rem;
+  }
+  .cell-content {
+    overflow-x: scroll;
+    height: 100%;
+    display: flex;
+    justify-content: flex-start;
+    position: relative;
+    white-space: nowrap;
+    .single-cell-content {
+      margin-right: 0.3rem;
+    }
+  }
 }
 </style>
